8cb80ad1d8524390bd9fc0229d9e5bb41d622491
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2013-12-26 13:25:33 +0900

    vDSPの使用、不使用を切り替えられるようにした。

--- AudioMeasurement/ViewController.m
+++ AudioMeasurement/ViewController.m
@@ -8,9 +8,12 @@
 
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import <Accelerate/Accelerate.h>
 
 @interface LevelMeter : UIView
 @property (nonatomic) float value;
+
+- (CGColorRef)valueToCGColorRef:(float)value;
 
 @end
 
@@ -19,6 +22,29 @@
     UILabel *label;
     NSMutableArray *history;
 }
+
+- (void)setValue:(float)value
+{
+    //NSLog(@"%s: value = %g", __FUNCTION__, value);
+    
+    label.text = [NSString stringWithFormat:@"%g", value];
+    [label sizeToFit];
+    
+    if (self.bounds.size.height < history.count) {
+        [history removeObjectAtIndex:0];
+    }
+    [history addObject:@(value)];
+    
+    [self setNeedsDisplay];
+}
+
+- (CGColorRef)valueToCGColorRef:(float)value
+{
+    UIColor *color = [UIColor colorWithHue:value saturation:1 brightness:1 alpha:1.0];
+    return color.CGColor;
+}
+
+#pragma mark override
 
 - (id)initWithCoder:(NSCoder *)aDecoder
 {
@@ -29,21 +55,6 @@
         history = [NSMutableArray array];
     }
     return self;
-}
-
-- (void)setValue:(float)value
-{
-    //NSLog(@"%s: value = %g", __FUNCTION__, value);
-    
-    label.text = [NSString stringWithFormat:@"%g", value];
-    [label sizeToFit];
-
-    if (self.bounds.size.height < history.count) {
-        [history removeObjectAtIndex:0];
-    }
-    [history addObject:@(value)];
-    
-    [self setNeedsDisplay];
 }
 
 - (void)drawRect:(CGRect)rect
@@ -60,15 +71,7 @@
     NSUInteger n = history.count;
     for (NSUInteger i = 0; i < n; i++) {
         float value = [history[n - i - 1] floatValue];
-        
-        // max: 0dB -> 0 (red)
-        // min: -60dB -> 0.5 (sky)
-        CGFloat h = 0.5 * MIN(-value, 60) / 60;
-        CGFloat s = 1.0;
-        CGFloat b = 1.0;
-        UIColor *color = [UIColor colorWithHue:h saturation:s brightness:b alpha:1.0];
-        CGContextSetStrokeColorWithColor(context, color.CGColor);
-        
+        CGContextSetStrokeColorWithColor(context, [self valueToCGColorRef:value]);
         CGFloat y = maxY - i;
         CGContextMoveToPoint(context, 0, y);
         CGContextAddLineToPoint(context, maxX, y);
@@ -78,13 +81,59 @@
 
 @end
 
+@interface dBLevelMeter : LevelMeter
+
+@end
+
+@implementation dBLevelMeter
+
+#pragma mark override
+
+- (CGColorRef)valueToCGColorRef:(float)value
+{
+    // max: 0dB -> 0 (red)
+    // min: -60dB -> 0.5 (sky)
+    CGFloat h = 0.5 * MIN(-value, 60) / 60;
+    CGFloat s = 1.0;
+    CGFloat b = 1.0;
+    UIColor *color = [UIColor colorWithHue:h saturation:s brightness:b alpha:1.0];
+    return color.CGColor;
+}
+
+@end
+
+@interface SampleLevelMeter : LevelMeter
+
+@end
+
+@implementation SampleLevelMeter
+
+#pragma mark override
+
+- (CGColorRef)valueToCGColorRef:(float)value
+{
+    // In absolute value:
+    // max: 10000 -> 0 (red)
+    // min: 0 -> 0.5 (sky)
+    CGFloat h = 0.5 * (1 - MIN(abs(value), 10000.0) / 10000);
+    CGFloat s = 1.0;
+    CGFloat b = 1.0;
+    UIColor *color = [UIColor colorWithHue:h saturation:s brightness:b alpha:1.0];
+    return color.CGColor;
+}
+
+@end
+
 @interface ViewController () <AVCaptureAudioDataOutputSampleBufferDelegate>
 @property (atomic) float average;
 @property (weak, nonatomic) IBOutlet LevelMeter *averageMeter;
 @property (atomic) float peak;
 @property (weak, nonatomic) IBOutlet LevelMeter *peakMeter;
+@property (atomic) float range;
+@property (weak, nonatomic) IBOutlet LevelMeter *rangeMeter;
 @property (atomic) BOOL running;
 @property (weak, nonatomic) IBOutlet UISlider *inputGainSlider;
+@property (weak, nonatomic) IBOutlet UISwitch *useVDSP;
 
 @end
 
@@ -102,6 +151,54 @@
         AVCaptureAudioChannel *channel = audioChannels[0];
         self.average = channel.averagePowerLevel;
         self.peak = channel.peakHoldLevel;
+        
+        // http://stackoverflow.com/questions/14088290/passing-avcaptureaudiodataoutput-data-into-vdsp-accelerate-framework
+        
+        // get a pointer to the audio bytes
+        CMItemCount numSamples = CMSampleBufferGetNumSamples(sampleBuffer);
+        CMBlockBufferRef audioBuffer = CMSampleBufferGetDataBuffer(sampleBuffer);
+        size_t lengthAtOffset;
+        size_t totalLength;
+        int16_t *samples;
+        CMBlockBufferGetDataPointer(audioBuffer, 0, &lengthAtOffset, &totalLength, (char**)&samples);
+        
+        // check what sample format we have
+        // this should always be linear PCM
+        // but may have 1 or 2 channels
+        CMAudioFormatDescriptionRef format = CMSampleBufferGetFormatDescription(sampleBuffer);
+        const AudioStreamBasicDescription *desc = CMAudioFormatDescriptionGetStreamBasicDescription(format);
+        assert(desc->mFormatID == kAudioFormatLinearPCM);
+        if (desc->mChannelsPerFrame == 1 && desc->mBitsPerChannel == 16) {
+
+            float min, max;
+            
+            if (self.useVDSP.on) {
+                float *convertedSamples = malloc(numSamples * sizeof(float));
+                vDSP_vflt16(samples, 1, convertedSamples, 1, numSamples);
+                vDSP_minv(convertedSamples, 1, &min, numSamples);
+                vDSP_maxv(convertedSamples, 1, &max, numSamples);
+                free(convertedSamples);
+            } else {
+                min = INFINITY;
+                max = -INFINITY;
+                for (int i = 0; i < numSamples; i++) {
+                    int16_t a = samples[i];
+                    if (a < min) {
+                        min = a;
+                    }
+                    if (max < a) {
+                        max = a;
+                    }
+                }
+            }
+            
+            self.range = max - min;
+
+        } else {
+            // handle other cases as required
+            NSLog(@"unexpected sampleBuffer format");
+        }
+        
         if (1 < audioChannels.count) {
             NSLog(@"warning: more than one audioChannels, ignored.");
         }
@@ -122,6 +219,10 @@
     } else if ([keyPath isEqualToString:@"peak"]) {
         dispatch_async(dispatch_get_main_queue(), ^{
             self.peakMeter.value = self.peak;
+        });
+    } else if ([keyPath isEqualToString:@"range"]) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.rangeMeter.value = self.range;
         });
     } else if ([keyPath isEqualToString:@"running"]) {
         self.inputGainSlider.enabled = self.running;
@@ -183,6 +284,10 @@
               context:nil];
     [self addObserver:self
            forKeyPath:@"peak"
+              options:NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew
+              context:nil];
+    [self addObserver:self
+           forKeyPath:@"range"
               options:NSKeyValueObservingOptionInitial|NSKeyValueObservingOptionNew
               context:nil];
     [self addObserver:self
